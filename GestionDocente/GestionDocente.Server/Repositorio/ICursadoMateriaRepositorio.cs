1ce78aaecf2cda6c9150a7ed67ea013fefa44828
Fiorella Rizzo <Rizzofiorella@outlook.com>
2024-12-09 22:25:44 -0300

    Controller Cursado Materia y CUPOF de profesor. DTO  Cursado Materia y DTO CUPOF de profe Repositorio e interface de repos de CursadoMateria y CUPOF

---
+++ GestionDocente/GestionDocente.Server/Repositorio/ICursadoMateriaRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using GestionDocente.BD.Data.Entity;
+using GestionDocente.Server.Repositorio;
+using GestionDocente.BD.Data.Entity;
+
+namespace GestionDocente.Server.Repositorio
+{
+    public interface ICursadoMateriaRepositorio : IRepositorio<CursadoMateria>
+    {
+        Task<List<CursadoMateria>> FullGetAll();
+        Task<CursadoMateria> FullGetById(int id);
+    }
+}
