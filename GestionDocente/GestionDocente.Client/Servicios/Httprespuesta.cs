c71e8bb0762e4cbe8085e6c70741404fc0203003
Janelith Rodriguez <janelithrodriguez@hotmail.com>
2024-12-09 23:50:00 -0300

    Inicio de primera Page

---
+++ GestionDocente/GestionDocente.Client/Servicios/Httprespuesta.cs
@@ -0,0 +1,16 @@
+﻿namespace GestionDocente.Client.Servicios
+{
+    public class HttpRespuesta<T>
+    {
+        public T Respuesta { get; }
+        public bool Error { get; set; }
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public HttpRespuesta(T respuesta, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Respuesta = respuesta;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+    }
+}
--- GestionDocente/GestionDocente.Server/Program.cs
+++ GestionDocente/GestionDocente.Server/Program.cs
@@ -9,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddJsonOptions(
     x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -35,9 +37,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
